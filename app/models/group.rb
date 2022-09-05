4bbe06302e7580cda077ec8e49a897abc919566a
Kelvin Bush <kelybush@gmail.com>
2022-09-05 19:30:07 +0300

    set up group entites many to many join table

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :group_entities
+  has_many :entities, through: :group_entities
 
   validates :name, presence: true, length: { in: 4..250 }
   validates :icon, presence: true, length: { in: 4..250 }
