e697f7f513769e82214d3b0b91f3233644816ff8
Kelvin Bush <kelybush@gmail.com>
2022-09-06 08:42:01 +0300

    add entities routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :group, only: [:index, :new, :create]
+  resources :group, only: [:index, :new, :create] do
+    resources :entities, only: [:index, :new, :create]
+  end
   root to: 'group#index'
 end
