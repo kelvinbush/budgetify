1dc60551cf188ed6f568d8f3936a3709a9b1cfc2
Kelvin Bush <kelybush@gmail.com>
2022-09-06 04:37:09 +0300

    set up group controller actions

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -2,14 +2,29 @@ class GroupController < ApplicationController
   load_and_authorize_resource
 
   def index
-
+    @groups = current_user.groups
   end
 
   def new
-
+    @user = current_user
+    @group = @user.groups.new
   end
 
   def create
+    @user = current_user
+    @group = @user.groups.create(group_params)
+    if @group.save
+      flash[:notice] = 'New category created successfully'
+      redirect_to group_index_path
+    else
+      flash.now[:alert] = 'Category creation failed'
+      render action: 'new'
+    end
+  end
 
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
